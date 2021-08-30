856c60858ad953caaf18dad1f93a1eed1b5fee8b
till straumann <till.straumann@alumni.tu-berlin.de>
2021-08-30 11:48:10 +0200

    top: hooked up FIFO block -- LEDs do not work anymore ?!

--- hdl/top.vhd
+++ hdl/top.vhd
@@ -17,23 +17,14 @@
 -- Additional Comments: 
 --
 ----------------------------------------------------------------------------------
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
-use IEEE.NUMERIC_STD.ALL;
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
-use work.psi_common_array_pkg.all;
-use work.psi_common_math_pkg.all;
-use work.psi_common_logic_pkg.all;
-
-use work.SimpleI2cMasterPkg.all;
+--library unisim;
+--use unisim.vcomponents.all;
 
 entity top is
    port (
@@ -85,8 +76,7 @@ end top;
 architecture rtl of top is
    
    constant NUM_LED_C : natural := 5;
-   constant CNT_LEN_C : natural := 28;
-   
+
    signal led         : std_logic_vector(NUM_LED_C - 1 downto 0) := (others => '1');
 
    signal fifoData_i  : std_logic_vector(7 downto 0);
@@ -103,191 +93,115 @@ architecture rtl of top is
    signal TP1_o       : std_logic := '0';
    signal TP1_i       : std_logic;
 
-   signal cnt         : unsigned(CNT_LEN_C - 1 downto 0) := to_unsigned( 0, CNT_LEN_C );
-   
    signal fifoClk     : std_logic;
    signal fifoRst     : std_logic := '0';
 
-   signal cmdVld      : std_logic;
-   signal cmdRdy      : std_logic;
-   signal cmdAck      : std_logic;
-   signal cmdDat      : std_logic_vector(7 downto 0);
-   signal cmdTyp      : std_logic_vector(2 downto 0);
+   signal sda_i       : std_logic;
+   signal sda_t       : std_logic := '1';
+   signal scl_i       : std_logic;
+   signal scl_t       : std_logic := '1';
 
-   signal rspDat      : std_logic_vector(7 downto 0);
-   signal rspVld      : std_logic;
-   signal rspAck      : std_logic;
-
-   component i2c_master is
-      generic (
-         RUNTIME_MODE_SEL : integer
-      );
-      port (
-         clk          : in  std_logic;
-         sda_in       : in  std_logic;
-         sda_out      : out std_logic;
-         scl_in       : in  std_logic;
-         scl_out      : out std_logic;
-
-         cmd          : in  std_logic_vector( 4 downto 0);
-         stat_out     : out std_logic_vector( 7 downto 0);
-         dat          : in  std_logic_vector( 7 downto 0);
-         dat_out      : out std_logic_vector( 7 downto 0);
-         ws           : in  std_logic;
-         rst          : in  std_logic;
-         mode         : in  std_logic_vector( 1 downto 0);
-         debug        : out std_logic_vector(15 downto 0)
-      );
-   end component i2c_master;
-
-   constant GEN_PSI_C : boolean := false;
+   signal cnt         : unsigned(23 downto 0) := (others => '0');
 
 begin
 
    fifoClk    <= IO_0;
 
-   -- FIFO
-   U_FIFO_BUF : entity work.IOBufArray
-      generic map (
-         WIDTH_G => 8
-      )
-      port map (
-         i     => fifoData_i,
-         o     => fifoData_o,
-         t     => fifoData_t,
-         io(0) => IO_L06_N_3,
-         io(1) => IO_L04_N_3,
-         io(2) => IO_L05_P_3,
-         io(3) => IO_L02_N_3,
-         io(4) => IO_L05_N_3,
-         io(5) => IO_L03_N_3,
-         io(6) => IO_L03_P_3,
-         io(7) => IO_L04_P_3
-      );
+--TS   -- FIFO
+--TS   U_FIFO_BUF : entity work.IOBufArray
+--TS      generic map (
+--TS         WIDTH_G => 8
+--TS      )
+--TS      port map (
+--TS         i     => fifoData_i,
+--TS         o     => fifoData_o,
+--TS         t     => fifoData_t,
+--TS         io(0) => IO_L06_N_3,
+--TS         io(1) => IO_L04_N_3,
+--TS         io(2) => IO_L05_P_3,
+--TS         io(3) => IO_L02_N_3,
+--TS         io(4) => IO_L05_N_3,
+--TS         io(5) => IO_L03_N_3,
+--TS         io(6) => IO_L03_P_3,
+--TS         io(7) => IO_L04_P_3
+--TS      );
+--TS
+--TS   IO_L01_P_3  <= fifoWR;
+--TS   IO_L01_N_3  <= fifoRDb;
+--TS   IO_L02_P_3  <= fifoSiWu;
+--TS   fifoTXFull  <= IP_3;
+--TS   fifoRXEmpty <= IO_L06_P_3;
 
-   IO_L01_P_3  <= fifoWR;
-   IO_L01_N_3  <= fifoRDb;
-   IO_L02_P_3  <= fifoSiWu;
-   fifoTXFull  <= IP_3;
-   fifoRXEmpty <= IO_L06_P_3;
-
---   IO_L08_N_2 <= led(4);
---   IO_L08_P_2 <= led(3);
+   IO_L08_N_2 <= led(4);
+   IO_L08_P_2 <= led(3);
    IO_L10_N_2 <= led(2);
---   IO_L09_N_2 <= led(1);
+   IO_L09_N_2 <= led(1);
    IO_L11_P_2 <= led(0);
---   
-   IO_L10_P_2 <= 'Z' when TP1_t = '1' else TP1_o;
-   TP1_i      <= IO_L10_P_2;
 
-GEN_PSI : if ( GEN_PSI_C ) generate
-   U_DUT : entity work.psi_common_i2c_master
-      generic map (
-         ClockFrequency_g => 12.0E6,
-         CmdTimeout_g     => 1.0e-3
-      )
-      port map (
-         Clk              => fifoClk,
-         Rst              => fifoRst,
-
-         CmdRdy           => cmdRdy,
-         CmdVld           => cmdVld,
-         CmdType          => cmdTyp,
-         CmdData          => cmdDat,
-         CmdAck           => cmdAck,
-
-         RspVld           => rspVld,
-         RspType          => open,
-         RspData          => rspDat,
-         RspAck           => rspAck,
-         RspArbLost       => open,
-         RspSeq           => open,
-
-         BusBusy          => open,
-         TimeoutCmd       => open,
-
-         I2cScl           => IO_L05_N_0,
-         I2cSda           => IO_L05_P_0,
-
-         I2cScl_I         => open,
-         I2cScl_O         => open,
-         I2cScl_T         => open,
-         I2cSda_I         => open,
-         I2cSda_O         => open,
-         I2cSda_T         => open
-      );
-
-   cmdVld      <= IO_L01_N_2;
-   IO_L02_N_2  <= cmdRdy;
-   cmdAck      <= IO_L03_P_2;
-
-   cmdTyp(2)   <= IO_L07_N_2;
-   cmdTyp(1)   <= IO_L08_P_2;
-   cmdTyp(0)   <= IO_L08_N_2;
-
-
-   rspVld      <= IO_L09_P_2;
-   rspAck      <= IO_L09_N_2;
-end generate GEN_PSI;
-
-GEN_VER : if ( not GEN_PSI_C ) generate
-   signal sda_o, scl_o : std_logic;
-
-   signal req          : I2cReqType;
-
-begin
-
-   IO_L05_P_0 <= 'Z' when sda_o = '1' else '0';
-   IO_L05_N_0 <= 'Z' when scl_o = '1' else '0';
-
-   req.start     <= IO_L09_P_2;
-   req.cmd(0)    <= IO_L09_N_2;
-   req.cmd(1)    <= IO_L07_N_2;
-   req.stop      <= IO_L08_P_2;
-   req.vld       <= IO_L08_N_2;
-   req.wdat      <= cmdDat;
-
-   IO_L02_N_2  <= cmdRdy;
-
-   U_TST : entity work.SimpleI2cMaster
-      generic map (
-         BUS_FREQ_HZ_G => 12.0E6
-      )
-      port map (
-         clk  => fifoClk,
-         rst  => fifoRst,
-         req  => req,
-         rdy  => cmdRdy,
-         rdat(0)          => IO_L03_P_2,
-         rdat(8 downto 1) => rspDat,
-         don  => IO_L01_N_2,
-
-         sclInp => IO_L05_N_0,
-         sclOut => scl_o,
-         sdaInp => IO_L05_P_0,
-         sdaOut => sda_o
-      );
-      
-
-end generate GEN_VER;
-
-   cmdDat(7)   <= IO_L04_P_2;
-   cmdDat(6)   <= IO_L03_N_2;
-   cmdDat(5)   <= IO_L04_N_2;
-   cmdDat(4)   <= IO_L05_P_2;
-   cmdDat(3)   <= IO_L06_P_2;
-   cmdDat(2)   <= IO_L05_N_2;
-   cmdDat(1)   <= IO_L06_N_2;
-   cmdDat(0)   <= IO_L07_P_2;
-
-   IO_L04_P_2  <= rspDat(7) when IO_2 = '1' else 'Z';
-   IO_L03_N_2  <= rspDat(6) when IO_2 = '1' else 'Z';
-   IO_L04_N_2  <= rspDat(5) when IO_2 = '1' else 'Z';
-   IO_L05_P_2  <= rspDat(4) when IO_2 = '1' else 'Z';
-   IO_L06_P_2  <= rspDat(3) when IO_2 = '1' else 'Z';
-   IO_L05_N_2  <= rspDat(2) when IO_2 = '1' else 'Z';
-   IO_L06_N_2  <= rspDat(1) when IO_2 = '1' else 'Z';
-   IO_L07_P_2  <= rspDat(0) when IO_2 = '1' else 'Z';
+--TS   IO_L10_P_2 <= 'Z' when TP1_t = '1' else TP1_o;
+--TS   TP1_i      <= IO_L10_P_2;
+--TS
+--TS   IO_L05_P_0 <= 'Z' when sda_t = '1' else '0';
+--TS   sda_i      <= IO_L05_P_0;
+--TS   IO_L05_N_0 <= 'Z' when scl_t = '1' else '0';
+--TS   scl_i      <= IO_L05_N_0;
+--TS
+--TS   GEN_FIFO_TST : if ( true ) generate
+--TS      signal fifoVld   : std_logic;
+--TS      signal fifoRDat  : std_logic_vector(7 downto 0);
+--TS      signal fifoRDatR : std_logic_vector(7 downto 0) := x"55";
+--TS      signal fifoRRdy  : std_logic := '1';
+--TS      signal fifoRVld  : std_logic;
+--TS
+--TS      signal fifoWDat  : std_logic_vector(7 downto 0) := x"00";
+--TS      signal fifoWRdy  : std_logic;
+--TS      signal fifoWVld  : std_logic := '0';
+--TS   begin
+--TS
+--TS   P_REG : process ( fifoClk ) is
+--TS   begin
+--TS      if ( rising_edge( fifoClk ) ) then
+--TS         if ( fifoVld = '1' ) then
+--TS            fifoRDatR <= fifoRDat;
+--TS         end if;
+--TS         cnt <= cnt + 1;
+--TS      end if;
+--TS   end process P_REG;
+--TS
+--TS   led(4 downto 2) <= fifoRDatR(4 downto 2);
+--TS   led(1)          <= '1';
+--TS   led(0)          <= cnt(23);
+--TS
+--TS   U_FIFO_IF : entity work.Ft240Fifo
+--TS      generic map (
+--TS         CLOCK_FREQ_G => 24.0E6
+--TS      )
+--TS      port map (
+--TS         clk     => fifoClk,
+--TS         rst     => fifoRst,
+--TS
+--TS         fifoRXE => fifoRXEmpty,
+--TS         fifoRDT => fifoData_i,
+--TS         fifoRDb => fifoRDb,
+--TS
+--TS         fifoTXF => fifoTXFull,
+--TS         fifoWDT => fifoData_o,
+--TS         fifoWR  => fifoWR,
+--TS
+--TS         fifoIOT => open,
+--TS
+--TS         rdat    => fifoRDat,
+--TS         rvld    => fifoRVld,
+--TS         rrdy    => fifoRRdy,
+--TS
+--TS         wdat    => fifoWDat,
+--TS         wvld    => fifoWVld,
+--TS         wrdy    => fifoWRdy
+--TS      );
+--TS
+--TS      fifoRRdy <= '1';
+--TS      fifoWVld <= '0';
+--TS
+--TS   end generate GEN_FIFO_TST;
 
 end rtl;
-
